afb610bffc1032448120745b576d29258dcd5679
jdazzle <areallycoolguy14@hotmail.com>
2018-01-28 11:32:01 -0500

    Updated page banners to use variable
    What the summary says.

--- themes/default/views/register.php
+++ themes/default/views/register.php
@@ -2,7 +2,7 @@
 
 	$output .= '
 
-		<div class="jumbotron jumbotron-fluid">
+		<div class="jumbotron jumbotron-fluid" style="background:linear-gradient(rgba(0,0,0,0.25), rgba(0,0,0,0.25)), url(\''.$bannerBackground.'\');background-repeat: no-repeat;background-size: cover;background-position: 50% 50%;">
 			<div class="container">
 		  		<h1 class="display-3">'.$frontpage_title.'</h1>
 		  	<p>'.$frontpage_message.'</p>
